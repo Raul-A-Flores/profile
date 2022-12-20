501027fb8458b0d5cf14b37fe4708a86faa9a6be
raul <raul.alexander.floress@gmail.com>
2022-12-20 13:55:32 -0600

    scss

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -37,7 +37,12 @@ const Header = () => (
       transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-img"
     >
-      <img src={images.profile} alt="profile_bg"  className='profile'/>
+      <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.profile}
+          alt="profile_bg"          className='profile'
+        />
       <motion.img
         whileInView={{ scale: [0, 1] }}
         transition={{ duration: 1, ease: 'easeInOut' }}
