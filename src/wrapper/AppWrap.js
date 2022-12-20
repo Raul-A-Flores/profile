01e780b0184d9c43b221baffe8d812fcfdeb64af
raul <raul.alexander.floress@gmail.com>
2022-12-20 17:32:13 -0600

    favicon

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -9,7 +9,7 @@ const AppWrap = (Component, idName, classNames) => function HOC(){
         <div className='app__wrapper app__flex'>
             <Component />
             <div className='copyright'>
-                <p className='p-text'>© 2022 Flores Capital Co.</p>
+                <p className='p-text'>© 2022 Flores </p>
                 <p className='p-text'>All rights reserved</p>
 
             </div>
